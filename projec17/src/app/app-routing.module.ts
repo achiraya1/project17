55ca487af3adcb9dd4047a113f0dde5e1bcd67c1
achiraya1 <khwaxchirya38@gmail.com>
2024-06-07 13:23:53 +0700

    first commit

--- projec17/src/app/app-routing.module.ts
+++ projec17/src/app/app-routing.module.ts
@@ -10,14 +10,6 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 ];
 
